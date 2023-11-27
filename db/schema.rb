108221204ca85c5a9f86487ca869dd35b5001bab
Tobias Groenland <cyber@tobiasgroenland.nl>
2023-11-27 16:54:09 +0100

    Location model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_142956) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_27_152915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string "address"
+    t.string "postal_code"
+    t.string "supermarkets_info"
+    t.string "schools_info"
+    t.string "parks_info"
+    t.string "nightlife_info"
+    t.string "restaurants_info"
+    t.string "transportation_info"
+    t.string "gym_info"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
