67ccd97df5bddc8c77620225ff81ddb404ae91fb
Elena Prousali <helenap_sal@yahoo.com>
2023-11-27 17:05:19 +0100

    created location and search models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_150420) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_27_155940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string "address"
+    t.string "postal_code"
+    t.integer "supermarkets_info"
+    t.integer "schools_info"
+    t.integer "parks_info"
+    t.integer "nightlife_info"
+    t.integer "restaurants_info"
+    t.integer "transportation_info"
+    t.integer "gym_info"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "location_id", null: false
+    t.integer "supermarket_score"
+    t.integer "schools_score"
+    t.integer "parks_score"
+    t.integer "nightlife_score"
+    t.integer "restaurants_score"
+    t.integer "transportation_score"
+    t.integer "gyms_score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_id"], name: "index_searches_on_location_id"
+    t.index ["user_id"], name: "index_searches_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,4 +66,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_150420) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "searches", "locations"
+  add_foreign_key "searches", "users"
 end
