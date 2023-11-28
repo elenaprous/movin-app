80f10bc2c8026a5706d3906ee67ff52717c0de64
Elena Prousali <helenap_sal@yahoo.com>
2023-11-28 17:35:43 +0100

    map view

--- db/migrate/20231127150420_add_columns_to_user.rb
+++ db/migrate/20231127150420_add_columns_to_user.rb
@@ -2,7 +2,7 @@ class AddColumnsToUser < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :important_address, :string
+    add_column :users, :important_addresses, :string
     add_column :users, :supermarkets_i, :integer
     add_column :users, :schools_i, :integer
     add_column :users, :parks_i, :integer
