501a00555487dcaa836f48ad40978075ccf6ae14
Sofia Bercianine <sfbercianine@gmail.com>
2023-11-28 16:59:17 +0100

    Started cache logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  CACHE = ActiveSupport::Cache::MemoryStore.new
+
   def configure_permitted_parameters
 
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :important_address,
