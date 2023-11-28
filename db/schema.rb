ddb5a6535a8f2d7b801d0345a2e9a047e9266a40
Sofia Bercianine <sfbercianine@gmail.com>
2023-11-28 12:42:27 +0100

    Created more components and advanced the home page

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,18 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_152915) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "searches", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_searches_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_searches_on_reset_password_token", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
