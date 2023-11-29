612fefbd2d9b57d3ff15dba920f2fd7fb78d639e
Sofia Bercianine <sfbercianine@gmail.com>
2023-11-29 10:31:31 +0100

    Created the initial preferences page, started the logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,8 +14,12 @@ class PagesController < ApplicationController
   end
 
   def preferences
-    if current_user.nil? && params[:address].present?
-      CACHE.write("address", params[:address], expires_in: 1.hour)
+    if current_user.nil? && params[:important_address].present?
+      CACHE.write("important_address", params[:important_address], expires_in: 1.hour)
+      CACHE.write("supermarkets_i", params[:supermarkets_i], expires_in: 1.hour)
+      CACHE.write("schools_i", params[:schools_i], expires_in: 1.hour)
+      CACHE.write("restaurants_i", params[:restaurants_i], expires_in: 1.hour)
+      CACHE.write("transportation_i", params[:transportation_i], expires_in: 1.hour)
     end
   end
 end
