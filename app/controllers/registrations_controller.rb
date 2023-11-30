8e9aa73caf9af376dffd2fe594906241f25374dd
Sofia Bercianine <sfbercianine@gmail.com>
2023-11-30 12:41:04 +0100

    Fixed the sign up redirect

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,15 +2,18 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     build_resource(sign_up_params)
 
-
     resource.save
-    Search.create(user_id: resource.id, location_id: session[:location_id])
     yield resource if block_given?
     if resource.persisted?
       if resource.active_for_authentication?
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
+        if session[:location_id]
+          @search = Search.create!(user_id: resource.id, location_id: session[:location_id])
+          redirect_to search_path(@search)
+        else
+          respond_with resource, location: after_sign_up_path_for(resource)
+        end
       else
         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
