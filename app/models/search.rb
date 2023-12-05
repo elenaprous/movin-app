a3a8b3b2ddce0a3277ae211b649fe597781e15f7
Elena Prousali <helenap_sal@yahoo.com>
2023-12-05 10:23:09 +0100

    refactored code in model search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,8 +1,29 @@
 class Search < ApplicationRecord
   belongs_to :user
   belongs_to :location
+  
+  after_save :compute_score_and_rank
 
-  #after_save :compute_score!, :compute_rank
+  def compute_score_and_rank
+    compute_score!()
+    compute_rank!()
+  end
+
+  private
+
+  def compute_rank!
+    columns = Search.columns.select { |col| col.name.include?("score") }
+    total   = 0
+
+    columns.each do |col|
+      column = col.name
+      value = self.send(column)
+
+      total += value
+    end
+
+    self.update(ranking: total)
+  end
 
   def compute_score!
     columns = Search.columns.select { |col| col.name.include?("score") }
@@ -12,24 +33,7 @@ class Search < ApplicationRecord
       score_from_user     = self.user.send(col) || 0
       score_from_location = self.location.send(col) || 0
 
-      puts "#{col} - #{score_from_user} - #{score_from_location}"
-
       self.update(col => score_from_user * score_from_location)
-      self.save!
     end
-  end
-
-  def compute_rank
-    columns = Search.columns.select { |col| col.name.include?("score") }
-    total   = 0
-
-    columns.each do |col|
-      column = col.name
-      value = self.send(column)
-      total += value
-    end
-
-    self.update(ranking: total)
-    self.save!
   end
 end
