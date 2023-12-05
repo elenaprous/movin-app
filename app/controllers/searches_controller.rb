d864fc2093be4b3f146736bb3fd18abba150d288
Sofia Bercianine <sfbercianine@gmail.com>
2023-12-05 12:56:35 +0100

    Added the locations to map, styled the popup

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,21 +2,18 @@ class SearchesController < ApplicationController
   def show
     @search = Search.find(params[:id])
 
-    # places = @search.location.pois
-    @markers = [
-      # places.geocoded.map do |place|
-      #   {
-      #     lat: place.lat,
-      #     lng: place.lon,
-      #     info_window_html: render_to_string(partial: "info_window", locals: { place: place })
-      #   }
-      # end,
+    @markers = @search.location.pois.map do |place|
       {
-        lat: @search.location.latitude,
-        lng: @search.location.longitude,
-        info_window_html: render_to_string(partial: "info_window", locals: { location: location })
+        lat: place.lat,
+        lng: place.lon,
+        info_window_html: render_to_string(partial: "poi_window", locals: { place: place })
       }
-    ]
+    end
+    @markers << {
+      lat: @search.location.latitude,
+      lng: @search.location.longitude,
+      info_window_html: render_to_string(partial: "info_window", locals: { location: location })
+    }
   end
 
   def index
