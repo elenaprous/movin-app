a71784cec533909ec146b2b0b09a8a2a50027a87
Tobias Groenland <cyber@tobiasgroenland.nl>
2023-12-07 15:50:51 +0100

    Fixed the extra address created when saved

--- app/javascript/controllers/insert_in_list_controller.js
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="insert-in-list"
 export default class extends Controller {
-  static targets = ["items", "form"]
+  static targets = ["items", "form", "input"]
 
   send(event) {
     event.preventDefault();
@@ -14,7 +14,11 @@ export default class extends Controller {
     })
       .then(response => response.json())
       .then((data) => {
-        this.itemsTarget.insertAdjacentHTML("beforeend", data.inserted_item)
+        if (data.address_add) {
+          this.itemsTarget.insertAdjacentHTML("beforeend", data.inserted_item)
+          this.element.querySelector('.mapboxgl-ctrl-geocoder--input').value = ''
+          this.inputTarget.value = '';
+        }
       })
   }
 }
