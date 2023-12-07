10f4ef1f0d8a1e111c8848db55702504e61b06e5
Tobias Groenland <cyber@tobiasgroenland.nl>
2023-12-07 10:52:48 +0100

    Implementation of slider values in searches#preferences page with complete fix #2

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -11,7 +11,7 @@ export default class extends Controller {
       accessToken: this.apiKeyValue,
       types: "country,region,place,postcode,locality,neighborhood,address",
       placeholder: "Give us an address",
-      limit: 7
+      limit: 4
     })
     this.geocoder.addTo(this.element)
     this.geocoder.on("result", event => this.setInputValue(event))
